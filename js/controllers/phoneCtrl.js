506f090722dab25458516672eeef62952875784f
Honehe13 <32700862+Honehe13@users.noreply.github.com>
2017-10-29 17:16:07 +0100

    Edit Delete Tooltip

--- js/controllers/phoneCtrl.js
+++ js/controllers/phoneCtrl.js
@@ -2,23 +2,33 @@
 app.controller('phoneCtrl', ['$scope', 'moment',  function($scope,moment) 
 {
 	
-	$scope.exampleDate = moment('2017-10-28').format('L');
-    $scope.entries = [
-	{name:'André',number:'05507401436',date:moment('2017-10-28').format('L')},
-	{name:'Peter',number:'0551501555',date:moment('2017-10-28').format('L')},
-	{name:'Frauke',number:'05518015',date:moment('2017-10-28').format('L')},
-	];
+    $scope.phonebook = 
+	{
+		entries:[
+	{id:1, name:'André',number:'05507401436',date:moment('2017-10-28').format('L')},
+	{id:2, name:'Peter',number:'0551501555',date:moment('2017-10-28').format('L')},
+	{id:3,name:'Frauke',number:'05518015',date:moment('2017-10-28').format('L')},
+	],
+	selected:{}
+	};
+	$scope.lastid = 3;
+	
 	$scope.showAdd = false;
 	$scope.inName = '';
 	$scope.inNumber = '';
+	$scope.showEditName = false;
+	$scope.showEditNumber = false;
 	
 	$scope.addItem = function()
 	{
 		if($scope.inName != '' && $scope.inNumber != '')
 		{
-			$scope.entries.push({name: $scope.inName,
+			$scope.lastid = $scope.lastid+1;
+			$scope.phonebook.entries.push({id: $scope.lastid,
+								name: $scope.inName,
 								number: $scope.inNumber,
 								date: moment().format('L')});
+			
 								
 			$scope.inName = '';
 			$scope.inNumber = '';
@@ -26,20 +36,48 @@ app.controller('phoneCtrl', ['$scope', 'moment',  function($scope,moment)
 								
 		}
 		$scope.showAdd = !$scope.showAdd;
-	}
+	};
 	
 	$scope.removeItem = function(x)
 	{
-		$scope.entries.splice(x,1); //entfernt ab X  "1" Item aus dem Array und gibt das array zurück.
-	}
+		$scope.phonebook.entries.splice(x,1); //entfernt ab X  "1" Item aus dem Array und gibt das array zurück.
+	};
 	
 	$scope.editName = function(x)
 	{
-		
-	}
+		$scope.phonebook.selected = angular.copy(x);
+		$scope.showEditName = true;
+	};
+	$scope.editNumber = function(x)
+	{
+		$scope.phonebook.selected = angular.copy(x);
+		$scope.showEditNumber = true;
+	};
+	
+	$scope.save = function(idx)
+	{
+		$scope.phonebook.entries[idx] = angular.copy($scope.phonebook.selected);
+		$scope.phonebook.selected = {};
+		$scope.showEditName = false;
+		$scope.showEditNumber = false;
+	};
 	
 	$scope.showAddItem = function()
 	{
 		$scope.showAdd = !$scope.showAdd;
-	}
+	};
+	
+	 // gets the template to ng-include for a table row / item
+    $scope.getTemplate = function (x)
+	{
+        if (x.id === $scope.phonebook.selected.id)
+		{
+			if($scope.showEditName)
+				return 'editName';
+			else
+				return 'editNumber';
+		}
+        else 
+			return 'display';
+    };
 }]);
